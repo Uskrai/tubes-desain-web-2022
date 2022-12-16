3b80947bf568da0463dda1bca1b7c40c315e618d
qonythazu <qonitaarif5@gmail.com>
2022-12-16 21:40:56 +0800

    Add Login

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import './App.css';
 import HomeScreen from './component/homeScreen';
 import RecipeScreen from "./component/recipeScreen";
+import Login from "./component/login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomeScreen />} />
       <Route path="/recipe/:id" element={<RecipeScreen />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
     // <div className="App bg-gray-600">
     //     <HomeScreen/>
