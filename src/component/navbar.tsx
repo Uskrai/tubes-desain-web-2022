653330d46b955fe1ed7aa5646560e4c4aa76b86a
istaqom <istaqom@gmail.com>
2022-12-17 09:58:33 +0800

    kelar login

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import { json } from "stream/consumers";
 
-export default class Navbar extends Component {
+export default class Navbar extends Component<any, any> {
+    
+    logout = () => {
+        localStorage.removeItem('user');
+        window.location.href = "/";
+    }
     render() {
+        const data:any = localStorage.getItem('user');
+        console.log(data);
+
         return (
             <header>
                 <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
@@ -24,12 +33,22 @@ export default class Navbar extends Component {
                                     <span className="sr-only">Search</span>
                                 </button>
                             </form>
-                            <a href="#" className="flex items-center pl-5">
-                                <svg className="flex-no-shrink fill-white h-6 w-6" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" >
+
+                            {/* <svg className="flex-no-shrink fill-white h-6 w-6" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" >
                                     <path d="M11.3 34.7q3.1-1.95 6.125-2.9T24 30.85q3.55 0 6.65 1 3.1 1 6.1 2.85 2.1-2.75 2.95-5.275.85-2.525.85-5.425 0-7-4.775-11.775T24 7.45q-7 0-11.775 4.775T7.45 24q0 2.9.85 5.4.85 2.5 3 5.3ZM24 25.85q-3.05 0-5.1-2.05t-2.05-5.1q0-3 2.05-5.075T24 11.55q3.05 0 5.1 2.075t2.05 5.125q0 3-2.05 5.05T24 25.85Zm0 19.4q-4.4 0-8.275-1.675T8.95 39q-2.9-2.9-4.55-6.75T2.75 24q0-4.4 1.675-8.275T9 8.975Q11.9 6.1 15.75 4.4q3.85-1.7 8.3-1.7 4.4 0 8.25 1.7 3.85 1.7 6.725 4.575 2.875 2.875 4.575 6.75Q45.3 19.6 45.3 24t-1.7 8.25Q41.9 36.1 39.025 39t-6.75 4.575Q28.4 45.25 24 45.25Z" />
-                                </svg>
-                                <span className="self-center text-xl whitespace-nowrap dark:text-white">Log In</span>
-                            </a>
+                                </svg> */}
+                            {/* <span className="self-center text-l whitespace-nowrap transition duration-300 ease-in-out dark:text-white hover:scale-110">Log In</span> */}
+                            {/* {login} */}
+                            {(data !== null) ? (
+                                <button className="flex items-center pl-5" onClick={this.logout}>
+                                    <span className="self-center text-l whitespace-nowrap transition duration-300 ease-in-out dark:text-white hover:scale-110">{data}</span>
+                                </button>
+                            ) : (
+                                <a href="/login" className="flex items-center pl-5">
+                                    <span className="self-center text-l whitespace-nowrap transition duration-300 ease-in-out dark:text-white hover:scale-110">Log In</span>
+                                </a>
+                            )}
+
                         </div>
                     </div>
                 </nav>
